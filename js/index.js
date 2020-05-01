455329c3d6b4572151e163b84ab7c911b7c07889
joooo24 <soany13@nate.com>
2020-05-01 18:38:39 +0900

    sub1 hj
    sub1 hj

--- js/index.js
+++ js/index.js
@@ -2,6 +2,22 @@
 
 ///////////// sub1
 
+$(".sub1_sect6 ul li").mouseenter(
+  function () {
+    $(this).find("img").addClass("on")
+    $(this).find(".sub1_name").css({ color: "#f8be03" })
+  },
+  1000,
+  function () {
+    $(".sub1_nametxt").addClass("on")
+  }
+)
+$(".sub1_sect6 ul li").mouseleave(function () {
+  $(this).find("img").removeClass("on")
+  $(this).find(".sub1_name").css({ color: "#fff" })
+})
+
+// background-color: #f8be03;
 ///////////// sub2
 
 ///////////// sub3
