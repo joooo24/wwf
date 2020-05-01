39f8d2a7c48a0053a7a1dd5c18a364a663895c57
son-max <thswlgus01@naver.com>
2020-05-01 19:17:59 +0900

    0501 sub1 작업
    작업중

--- js/index.js
+++ js/index.js
@@ -3,7 +3,35 @@
 ///////////// sub1
 
 ///////////// sub2
-
+var swiper = new Swiper('.sub2-sect2 .swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    speed: 700,
+    pagination: {
+      el: '.sub2-sect2 .swiper-pagination',
+      clickable: false,
+    },
+    navigation: {
+      nextEl: '.sub2-sect2 .swiper-button-next',
+      prevEl: '.sub2-sect2 .swiper-button-prev',
+    },
+  });
+  var sw = 0;
+  $('.sub2-sect2 .swiper-button-next').click(function(){
+    sw += 1;
+    if(sw==1){
+        $('.animalInfo').removeClass('on')
+        $('.animalInfo:nth-child(2)').addClass('on')
+    } else if(sw==2){
+        $('.animalInfo').removeClass('on')
+        $('.animalInfo:nth-child(3)').addClass('on')
+    }console.log(sw)
+  })
+  $('.sub2-sect2 .swiper-button-prev').click(function(){
+    sw -= 1;
+    console.log(sw)
+  })
 ///////////// sub3
 
 ///////////// sub4
