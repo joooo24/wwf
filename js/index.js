81063c11ab6f7c4bd255f48e2cc946a6c2a962b5
mihyun <k8768352@naver.com>
2020-05-01 19:19:30 +0900

    0501 mihyun
    talk.html 진행 중
    js, css, html 진행 중 /  반응형 해야함
    img 작업 필요

--- js/index.js
+++ js/index.js
@@ -6,4 +6,12 @@
 
 ///////////// sub3
 
+$('.S3-name li').click(function(e){
+    e.preventDefault()
+    var S3in = $(this).index()
+    $(this).closest('section').find('.S3-m-title li').removeClass('on')
+    $(this).closest('section').find('.S3-m-title li').removeClass('on')
+    $(this).closest('section').find('.S3-m-title li').eq(S3in).addClass('on')
+})
+
 ///////////// sub4
