f787c2e5fcb80d7d0d01d7d2ca722abe402b1e76
joooo24 <soany13@nate.com>
2020-05-06 17:17:59 +0900

    sub1
    sub1

--- js/index.js
+++ js/index.js
@@ -2,17 +2,27 @@
 
 ///////////// sub1
 
-$(".sub1_sect6 ul li").mouseenter(function () {
-  $(this).find("img").addClass("on")
-  $(this)
-    .find(".sub1_name")
-    .animate({ color: "#f8be03" }, function () {
-      $(this).closet(".sub1_nametxt").animate({ backgroundColor: "#f8be03" })
-    })
-})
-$(".sub1_sect6 ul li").mouseleave(function () {
-  $(this).find("img").removeClass("on")
-  $(this).find(".sub1_name").css({ color: "#fff" })
+// $(".sub1_sect6 ul li").mouseenter(function () {
+//   $(this).find(".sub1_name").hide()
+//   $(this).find(".sub1_active").show()
+//   $(".sub1_nametxt").removeClass("on")
+//   $(this).find(".sub1_nametxt").addClass("on")
+// })
+// $(".sub1_sect6 ul li").mouseleave(function () {
+//   $(this).find(".sub1_name").show()
+//   $(this).find(".sub1_active").hide()
+//   $(".sub1_nametxt").removeClass("on")
+// })
+
+$(".history_tab a").click(function (e) {
+  e.preventDefault()
+  var idx = $(this).parent("li").index()
+  $(".history_con img").stop().fadeOut()
+  $(".history_con img").eq(idx).stop().fadeIn()
+  $(".history_txt").stop().fadeOut()
+  $(".history_txt").eq(idx).stop().fadeIn()
+  $(".history_tab a").removeClass("on")
+  $(this).addClass("on")
 })
 
 ///////////// sub2
