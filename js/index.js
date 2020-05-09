771f724434958f144bcd16e706b1019607382634
mihyun <k8768352@naver.com>
2020-05-09 17:07:37 +0900

    mihyun0509
    반응형때문에 다시 하는 중

--- js/index.js
+++ js/index.js
@@ -30,55 +30,55 @@ $(".history_tab a").click(function (e) {
 // $(window).scroll()
 
 ///////////// sub2
-var swiper = new Swiper('.sub2-sect2 .swiper-container', {
-    slidesPerView: 1,
-    spaceBetween: 0,
-    loop: true,
-    speed: 700,
-    pagination: {
-      el: '.sub2-sect2 .swiper-pagination',
-      clickable: false,
-    },
-    navigation: {
-      nextEl: '.sub2-sect2 .swiper-button-next',
-      prevEl: '.sub2-sect2 .swiper-button-prev',
-    },
-  });
-  var sw = 0;
-  $('.sub2-sect2 .swiper-button-next').click(function(){
-    sw += 1;
-    if(sw==1){
-        $('.animalInfo').removeClass('on')
-        $('.animalInfo:nth-child(2)').addClass('on')
-    } else if(sw==2){
-        $('.animalInfo').removeClass('on')
-        $('.animalInfo:nth-child(3)').addClass('on')
-    }else if(sw==3){
-      $('.animalInfo').removeClass('on')
-      $('.animalInfo:nth-child(4)').addClass('on')
-  }console.log(sw)
-  })
-  $('.sub2-sect2 .swiper-button-prev').click(function(){
-    sw -= 1;
-    console.log(sw)
-  })
+var swiper = new Swiper(".sub2-sect2 .swiper-container", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  loop: true,
+  speed: 700,
+  pagination: {
+    el: ".sub2-sect2 .swiper-pagination",
+    clickable: false,
+  },
+  navigation: {
+    nextEl: ".sub2-sect2 .swiper-button-next",
+    prevEl: ".sub2-sect2 .swiper-button-prev",
+  },
+})
+var sw = 0
+$(".sub2-sect2 .swiper-button-next").click(function () {
+  sw += 1
+  if (sw == 1) {
+    $(".animalInfo").removeClass("on")
+    $(".animalInfo:nth-child(2)").addClass("on")
+  } else if (sw == 2) {
+    $(".animalInfo").removeClass("on")
+    $(".animalInfo:nth-child(3)").addClass("on")
+  } else if (sw == 3) {
+    $(".animalInfo").removeClass("on")
+    $(".animalInfo:nth-child(4)").addClass("on")
+  }
+  console.log(sw)
+})
+$(".sub2-sect2 .swiper-button-prev").click(function () {
+  sw -= 1
+  console.log(sw)
+})
 ///////////// sub3
 
-$('.S3-name li').click(function(e){
-    e.preventDefault()
-    $(this).parents().children().removeClass('on')
-    $(this).addClass('on')
+$(".S3-name li").click(function (e) {
+  e.preventDefault()
+  $(this).parents().children().removeClass("on")
+  $(this).addClass("on")
 
-    var S3in = $(this).index()
-    $(this).closest('section').find('.S3-m-title li').removeClass('on')
-    $(this).closest('section').find('.S3-m-title li').eq(S3in).addClass('on')
-    $(this).closest('section').find('.S3-video iframe').removeClass('on')
-    $(this).closest('section').find('.S3-video iframe').eq(S3in).addClass('on')
+  var S3in = $(this).index()
+
+  $(this).closest("section").find(".S3-sect02-yellowBox li").removeClass("on")
+  $(this).closest("section").find(".S3-sect02-yellowBox li").eq(S3in).addClass("on")
+  $(this).closest("section").find(".S3-video iframe").removeClass("on")
+  $(this).closest("section").find(".S3-video iframe").eq(S3in).addClass("on")
 })
 
 ///////////// sub4
-
-
 
 // support
 
